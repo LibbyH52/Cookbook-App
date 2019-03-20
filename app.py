1680f2b8c6d407188f444bbd67d458281c4a2833
Libby Henry <libbyh52@gmail.com>
2019-03-20 13:09:44 +0000

    Created browse_recipes function and got recipes to display to screen

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import pymongo
 import os
-from flask import Flask
-#from config import * 
+from flask import Flask, render_template, redirect, request, url_for #check meaning
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+import config
 
-# use os library to set constant
-#MONGODB_URI = os.getenv("MONGO_URI")
 app = Flask(__name__)
 
+# use os library to set constant
+app.config['MONGO_URI'] = config.MONGO_URI
+app.config['DB_NAME'] = config.DB_NAME
+
+#creating a new instance of PyMongo and going to add the app object into that with a constructor method
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/browse_recipes')
+def browse_recipes():
+    return render_template('browseRecipes.html', recipes=mongo.db.recipes.find())
+        
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')),
