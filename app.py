e6d762f48ed0301b3793d6f0beccb53930592078
Libby Henry <libbyh52@gmail.com>
2019-04-08 20:29:24 +0000

    disable 'Delete Button' for testing rest of site

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def insert_recipe():
 #deletes a recipe
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
-    mongo.db.recipes.remove({'_id':ObjectId(recipe_id)})
+    #mongo.db.recipes.remove({'_id':ObjectId(recipe_id)})
     return redirect(url_for('browse_recipes'))
     
 if __name__ == '__main__':
