ea443c08ed3d4c8364e382a9f1bb7d63f5585980
Libby Henry <libbyh52@gmail.com>
2019-04-17 14:49:18 +0000

    added popup modal to displayrecipe page to allow user to confirm delete, styled modal and removed jQuery confirm function from script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
-    $(".navbar-nav").click(function() {
-        $(".navbar-collapse").collapse("toggle")
-    });
-    $(".new-input-btn").on('click', function() {
-        $('<input type="text" class="form-control" name="ingredient" id="ingredient" placeholder="20g plain flour sieved" required >').insertBefore(".new-input-btn");
-    });
-    $(".remove-input-btn").on('click', function() {
-        $('#ingredients-row input:last').remove();
-    });
-    $(".new-recipe").submit(function() {
-        alert("Thank you! Your recipe has been added.");
-    });
-    $(".edit-recipe").submit(function() {
-        alert("Thank you! This recipe has now been updated.");
-    });
-    $(".btn-filter").attr("disabled", true)
-    $(".form-check-input").change(function() {
-        $(".btn-filter").attr("disabled", $(".form-check-input").length == 0)
-    });
-});
+      $(document).ready(function(){
+          $(".navbar-nav").click(function() {
+              $(".navbar-collapse").collapse("toggle")
+          });
+          $(".new-input-btn").on('click', function(){
+            $('<input type="text" class="form-control" name="ingredient" id="ingredient" placeholder="20g plain flour sieved" required >').insertBefore(".new-input-btn");
+          });
+          $(".remove-input-btn").on('click', function(){
+              $('#ingredients-row input:last').remove();
+          });
+          $(".new-recipe").submit(function(){
+              alert("Thank you! Your recipe has been added.");
+          });
+          $(".edit-recipe").submit(function(){
+              alert("Thank you! This recipe has now been updated.");
+          });
+         $('.btn-filter').attr("disabled",true);
+            $('.form-check-input').change(function() {
+            $('.btn-filter').attr('disabled', $('.form-check-input:checked').length == 0);
+        });
+     });
