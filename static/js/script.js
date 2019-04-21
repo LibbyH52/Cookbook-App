c561048de72273cd8a32fd8079dd312f0655c23d
Libby Henry <libbyh52@gmail.com>
2019-04-21 20:06:35 +0000

    js file accidently deleted adding code from prvious commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,17 @@
-      $(document).ready(function(){
-          $(".navbar-nav").click(function() {
-              $(".navbar-collapse").collapse("toggle")
-          });
-          $(".new-input-btn").on('click', function(){
-            $('<input type="text" class="form-control" name="ingredient" id="ingredient" placeholder="20g plain flour sieved" required >').insertBefore(".new-input-btn");
-          });
-          $(".remove-input-btn").on('click', function(){
-              $('#ingredients-row input:last').remove();
-          });
-          $(".new-recipe").submit(function(){
-              alert("Thank you! Your recipe has been added.");
-          });
-          $(".edit-recipe").submit(function(){
-              alert("Thank you! This recipe has now been updated.");
-          });
-         /*$('.btn-filter').prop("disabled", true);*/
-            
-     });
+$(document).ready(function() {
+    $(".navbar-nav").click(function() {
+        $(".navbar-collapse").collapse("toggle")
+    });
+    $(".new-input-btn").on('click', function() {
+        $('<input type="text" class="form-control" name="ingredient" id="ingredient" placeholder="20g plain flour sieved" required >').insertBefore(".new-input-btn");
+    });
+    $(".remove-input-btn").on('click', function() {
+        $('#ingredients-row input:last').remove();
+    });
+    $(".new-recipe").submit(function() {
+        alert("Thank you! Your recipe has been added.");
+    });
+    $(".edit-recipe").submit(function() {
+        alert("Thank you! This recipe has now been updated.");
+    });
+});
