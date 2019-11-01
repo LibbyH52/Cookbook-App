6b8f37563714cd0c40c0d5d21151b7e15b23f2b7
Libby Henry <libbh52@gmail.com>
2019-11-01 15:45:25 +0000

    Temporarily disable edit and delete buttons

--- app.py
+++ app.py
@@ -54,43 +54,43 @@ def browse_recipes():
 def display_recipe(recipe_id):
     return render_template('displayrecipe.html', recipe=mongo.db.recipes.find_one({'_id':ObjectId(recipe_id)}))
 
-#selects a recipe and retreives from the database using its id and displays it in a form to allow the user to edit its properties
-@app.route('/edit_recipe/<recipe_id>')
-def edit_recipe(recipe_id):
-    courses = mongo.db.course.find()
-    #get the recipe that matches the recipe id '_id' is the key 
-    recipe = mongo.db.recipes.find_one({'_id':ObjectId(recipe_id)})
-    return render_template("editrecipe.html", recipe=recipe, courses=courses, allergens=mongo.db.allergens.find())
+# #selects a recipe and retreives from the database using its id and displays it in a form to allow the user to edit its properties
+# @app.route('/edit_recipe/<recipe_id>')
+# def edit_recipe(recipe_id):
+#     courses = mongo.db.course.find()
+#     #get the recipe that matches the recipe id '_id' is the key 
+#     recipe = mongo.db.recipes.find_one({'_id':ObjectId(recipe_id)})
+#     return render_template("editrecipe.html", recipe=recipe, courses=courses, allergens=mongo.db.allergens.find())
 
-#updates the database with edited recipe
-@app.route('/update_recipe/<recipe_id>', methods=["GET", "POST"])
-def update_recipe(recipe_id):
-    recipes = mongo.db.recipes
-    #access recipes collection and call the update function
-    recipes.update({'_id':ObjectId(recipe_id)},
-        #match form fields to keys in the recipes collection
-        {
-            'recipe_name': request.form.get('recipe_name'),
-            'image_url' :request.form.get('image_url'),
-            'author' :request.form.get('author'),
-            'course_name': request.form.get('course'),
-            'cuisine_name': request.form.get('cuisine'),
-            'servings':int(request.form.get('servings')),
-            'prep_time':request.form.get('prep_time'),
-            'cook_time':request.form.get('cook_time'),
-            'allergens':request.form.getlist('allergen'),
-            'ingredients':request.form.getlist('ingredient'),
-            'instructions':request.form.get('instructions')
-        })
-    cuisine=request.form.get('cuisine')
-    if not cuisine == "":
-        cuisine = mongo.db.cuisine.update(
-            {"cuisine_name": cuisine},
-            { "$setOnInsert": { "cuisine_name": cuisine },
-            },
-            upsert= True 
-        ); 
-    return redirect(url_for('browse_recipes'))
+# #updates the database with edited recipe
+# @app.route('/update_recipe/<recipe_id>', methods=["GET", "POST"])
+# def update_recipe(recipe_id):
+#     recipes = mongo.db.recipes
+#     #access recipes collection and call the update function
+#     recipes.update({'_id':ObjectId(recipe_id)},
+#         #match form fields to keys in the recipes collection
+#         {
+#             'recipe_name': request.form.get('recipe_name'),
+#             'image_url' :request.form.get('image_url'),
+#             'author' :request.form.get('author'),
+#             'course_name': request.form.get('course'),
+#             'cuisine_name': request.form.get('cuisine'),
+#             'servings':int(request.form.get('servings')),
+#             'prep_time':request.form.get('prep_time'),
+#             'cook_time':request.form.get('cook_time'),
+#             'allergens':request.form.getlist('allergen'),
+#             'ingredients':request.form.getlist('ingredient'),
+#             'instructions':request.form.get('instructions')
+#         })
+#     cuisine=request.form.get('cuisine')
+#     if not cuisine == "":
+#         cuisine = mongo.db.cuisine.update(
+#             {"cuisine_name": cuisine},
+#             { "$setOnInsert": { "cuisine_name": cuisine },
+#             },
+#             upsert= True 
+#         ); 
+#     return redirect(url_for('browse_recipes'))
     
     
 # displays a form that allows the user to add a recipe to the database (only partially complete)
@@ -129,11 +129,11 @@ def insert_recipe():
             ); 
     return redirect(url_for('browse_recipes'))
     
-#deletes a recipe
-@app.route('/delete_recipe/<recipe_id>')
-def delete_recipe(recipe_id):
-    mongo.db.recipes.remove({'_id':ObjectId(recipe_id)})
-    return redirect(url_for('browse_recipes'))
+# #deletes a recipe
+# @app.route('/delete_recipe/<recipe_id>')
+# def delete_recipe(recipe_id):
+#     mongo.db.recipes.remove({'_id':ObjectId(recipe_id)})
+#     return redirect(url_for('browse_recipes'))
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')),
